c8e11acfcbea5fab3bf6e163fcb6543188b22ac0
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-11-01 17:45:45 +0900

    feat: ChatRepository 클래스 추가

---
+++ backend/src/main/java/com/dwbh/backend/repository/chat/ChatRepository.java
@@ -0,0 +1,7 @@
+package com.dwbh.backend.repository.chat;
+
+import com.dwbh.backend.entity.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+}
