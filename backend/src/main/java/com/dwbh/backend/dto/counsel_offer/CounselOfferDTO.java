9ff22e73379387538dac711444feca6b7c6a30cd
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-11-04 18:03:42 +0900

    feat: 댓글 연관관계 추가

---
+++ backend/src/main/java/com/dwbh/backend/dto/counsel_offer/CounselOfferDTO.java
@@ -0,0 +1,19 @@
+package com.dwbh.backend.dto.counsel_offer;
+
+import com.dwbh.backend.common.entity.YnType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CounselOfferDTO {
+    private Long counselOfferSeq;
+    private Long counselorHireSeq;
+    private Long userSeq;
+
+    private String counselOfferContent;
+    private YnType counselOfferPrivateYn;
+    private LocalDateTime counselOfferDelDate;
+}
