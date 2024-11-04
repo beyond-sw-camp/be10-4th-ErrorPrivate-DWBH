0279f35bb616a79709fad9545f00cda925ce001e
dispear <kkm7hjh@naver.com>
2024-11-04 15:40:04 +0900

    test: swagger 작성

--- backend/src/main/java/com/dwbh/backend/controller/evaluation/EvaluationController.java
+++ backend/src/main/java/com/dwbh/backend/controller/evaluation/EvaluationController.java
@@ -3,12 +3,15 @@ package com.dwbh.backend.controller.evaluation;
 import com.dwbh.backend.dto.evaluation.EvaluationRequest;
 import com.dwbh.backend.dto.evaluation.EvaluationResponse;
 import com.dwbh.backend.service.evaluation.EvaluationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "평가 컨트롤러", description = "평가 조회/작성/수정/삭제")
 @RestController(value = "evaluationController")
 @RequiredArgsConstructor    // final을 받은 필드의 생성자를 주입
 @RequestMapping("/api/v1")
@@ -18,6 +21,7 @@ public class EvaluationController {
     private final EvaluationService evaluationService;
 
     // 평가 수정을 위한 조회
+    @Operation(summary = "평가 조회", description = "채팅방번호에 해당하는 평가를 조회한다.")
     @GetMapping("/chat/{chatSeq}/evaluation")
     public ResponseEntity<EvaluationResponse> readEvaluation(@PathVariable("chatSeq") Long chatSeq) {
 
@@ -27,6 +31,7 @@ public class EvaluationController {
     }
 
     // 평가 작성
+    @Operation(summary = "평가 작성", description = "작성되지 않은 평가를 작성한다.")
     @PostMapping("/evaluation")
     public ResponseEntity<String> createEvaluation(
             @RequestBody EvaluationRequest evaluationRequest
@@ -37,6 +42,7 @@ public class EvaluationController {
     }
 
     // 평가 수정
+    @Operation(summary = "평가 수정", description = "이미 작성된 평가를 수정한다.")
     @PutMapping("/evaluation/{evaluationSeq}")
     public ResponseEntity<String> updateEvaluation(
             @PathVariable Long evaluationSeq,
@@ -48,6 +54,7 @@ public class EvaluationController {
     }
 
     // 평가 삭제
+    @Operation(summary = "평가 삭제", description = "이미 작성된 평가를 삭제한다.")
     @DeleteMapping("/evaluation/{evaluationSeq}")
     public ResponseEntity<String> deleteEvaluation(
             @PathVariable Long evaluationSeq
