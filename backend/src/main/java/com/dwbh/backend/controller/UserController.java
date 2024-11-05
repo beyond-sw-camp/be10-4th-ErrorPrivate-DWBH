81ec2e90003470874d85d31bdece77219ef891e0
hcbak <hc.bak@outlook.com>
2024-11-05 15:02:45 +0900

    docs: 스웨거 설정 추가

--- backend/src/main/java/com/dwbh/backend/controller/UserController.java
+++ backend/src/main/java/com/dwbh/backend/controller/UserController.java
@@ -2,6 +2,8 @@ package com.dwbh.backend.controller;
 
 import com.dwbh.backend.dto.CreateUserRequest;
 import com.dwbh.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Tag(name = "User", description = "회원")
 public class UserController {
 
     private final UserService userService;
 
     // 회원가입
     @PostMapping("/user")
+    @Operation(summary = "회원 등록", description = "회원을 등록한다.")
     public ResponseEntity<Void> createUser(
             @Validated @RequestBody CreateUserRequest createUserRequest) {
 
