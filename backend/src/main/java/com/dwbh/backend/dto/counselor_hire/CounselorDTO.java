a4d796332bf2db1f0c66523a296479c7d660e739
sung-mini <sungmini9755@naver.com>
2024-11-06 11:10:54 +0900

    chore: 오타로 인한 수정

--- backend/src/main/java/com/dwbh/backend/dto/counselor_hire/CounselorDTO.java
+++ backend/src/main/java/com/dwbh/backend/dto/counselor_hire/CounselorDTO.java
@@ -1,15 +1,15 @@
-package com.dwbh.backend.dto.counselor_hire;
+    package com.dwbh.backend.dto.counselor_hire;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+    import jakarta.validation.constraints.NotBlank;
+    import lombok.*;
 
-@Getter
-@Setter
-public class CounselorDTO {
-    @NotBlank
-    private String counselorHireTitle;
-    @NotBlank
-    private String counselorHireContent;
-    @NotBlank
-    private String counselorHireCounselorGender;
-}
+    @Getter
+    @Setter
+    public class CounselorDTO {
+        @Getter
+        private String counselorHireTitle;
+        @Getter
+        private String counselorHireContent;
+        @Getter
+        private String counselorHireCounselorGender;
+    }
