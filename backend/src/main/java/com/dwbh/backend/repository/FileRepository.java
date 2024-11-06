b84d2d6582925798a0890070558239cdb0c84a4a
catnine11 <catnine11@gmail.com>
2024-11-06 18:14:53 +0900

    feat: 소프트 딜리트 구현 refactoring: 폴더 이동

--- backend/src/main/java/com/dwbh/backend/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dwbh.backend.repository;
-
-import com.dwbh.backend.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository  extends JpaRepository<File, Long> {
-}
