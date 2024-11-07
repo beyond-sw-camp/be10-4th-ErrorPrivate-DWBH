0bdab42a0571934c4aaa411ee4337cba9a42e01e
AYeong-Jeon <ay98923@naver.com>
2024-11-07 19:05:03 +0900

    feat: 채팅 rabbitmq 삭제

--- backend/src/main/java/com/dwbh/backend/dto/chat/ChatMessageDTO.java
+++ backend/src/main/java/com/dwbh/backend/dto/chat/ChatMessageDTO.java
@@ -1,12 +1,22 @@
 package com.dwbh.backend.dto.chat;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter @Setter
 public class ChatMessageDTO {
 
-    private String roomId;
-    private String writer;
+    private Long seq;
+    private Long chatRoomSeq;
+    private Long memberSeq;
+
     private String message;
+    private String region;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime regDate;
 
 }
