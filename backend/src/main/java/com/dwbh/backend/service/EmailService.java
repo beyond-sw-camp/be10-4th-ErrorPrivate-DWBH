afcc843a66cce11f6f4bd4463e33d211f3c192ae
Huichan Bak <hc.bak@outlook.com>
2024-11-09 14:54:58 +0900

    feat: 이메일 인증 정보 청소 로봇 설치

--- backend/src/main/java/com/dwbh/backend/service/EmailService.java
+++ backend/src/main/java/com/dwbh/backend/service/EmailService.java
@@ -4,15 +4,18 @@ import com.dwbh.backend.common.util.RandomStringGeneratorUtil;
 import com.dwbh.backend.dto.user.SendEmailRequest;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.messaging.MessagingException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailService {
@@ -20,12 +23,12 @@ public class EmailService {
     private final JavaMailSender javaMailSender;
     private final Map<String, Pair> fakeInMemoryDB = new HashMap<>();
 
+    // 이메일 인증 메일 전송
     public void sendEmail(SendEmailRequest sendEmailRequest) {
 
         String verifyKey = RandomStringGeneratorUtil.generateRandomString(10);
         Pair pair = new Pair(verifyKey, LocalDateTime.now().plusMinutes(10));
         String body = "DWBH 이메일 인증 번호입니다.<br>" + verifyKey;
-        System.out.println("body = " + body);
         fakeInMemoryDB.put(sendEmailRequest.getEmail(), pair);
 
         try {
@@ -44,6 +47,7 @@ public class EmailService {
         }
     }
 
+    // 이메일 인증 코드 검증
     public String verifyEmail(String email, String code) {
         Pair pair = fakeInMemoryDB.get(email);
 
@@ -51,6 +55,19 @@ public class EmailService {
             fakeInMemoryDB.get(email).verify = true;
             return "Verify Success";
         } else { return "Verify Fail"; }
+    }
+
+    // FakeInMemoryDB 청소
+    @Scheduled(fixedDelay = 60000)
+    public void cleanFakeInMemoryDB() {
+        log.info("이메일 인증 정보 청소하려 왔어요~");
+
+        for (Map.Entry<String, Pair> entry : fakeInMemoryDB.entrySet()) {
+            if (entry.getValue().expire.isBefore(LocalDateTime.now())) {
+                fakeInMemoryDB.remove(entry.getKey());
+                log.info("청소 당한 키: " + entry.getKey());
+            }
+        }
     }
 }
 
