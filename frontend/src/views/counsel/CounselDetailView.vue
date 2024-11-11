6bb3d561fddbb4d529f824e84d5d65aa6acbf340
beanteacher <dh65432@naver.com>
2024-11-12 03:55:26 +0900

    feat: 수정 삭제 버튼 생성 / insert 쪽 못했음 개빡침

--- frontend/src/views/counsel/CounselDetailView.vue
+++ frontend/src/views/counsel/CounselDetailView.vue
@@ -5,6 +5,7 @@ import dayjs from "dayjs";
 import "@/css/style.css"
 import {useRoute, useRouter} from "vue-router";
 import CounselCommentSection from "@/components/counseloffer/CounselCommentSection.vue";
+import {useAuthStore} from "@/stores/auth.js";
 
 const router = useRouter();
 // 라우터로 온 데이터 받기
@@ -16,6 +17,10 @@ const counselHire = ref([]);
 const counselorAgeRanges = ref([]);
 const counselorTypes = ref([]);
 const sendUserSeq = ref(0);
+
+const authStore = useAuthStore();
+const userSeq = authStore.userSeq;
+
 const fetchCounselHires = async () => {
   try {
     const response = await axios.get(`http://localhost:8089/api/v1/counselor-hire/${hireSeq}`);
@@ -55,45 +60,40 @@ const goToList = () => {
         <p>
           {{ counselHire.hireContent }}
         </p>
-
-        <!-- 이미지 -->
-  <!--      <img
-            src="/path/to/your/image.jpg"
-            alt="상담 이미지"
-            class="img-fluid rounded mt-4"
-        />-->
-
-        <!-- 추가 정보 -->
         <table class="table mt-4">
           <tbody>
-          <tr>
-            <th scope="row">희망 상담사 나이</th>
-            <td>
-              <template v-if="counselorAgeRanges.length > 0" v-for="(counselorAgeRange, index) in counselorAgeRanges" :key="index">{{counselorAgeRange}}</template>
-              <template v-else>무관</template>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">희망 성별</th>
-            <td>{{ counselHire.hireGender }}</td>
-          </tr>
-          <tr>
-            <th scope="row">희망 조언 타입</th>
-            <td>
-              <template v-if="counselorTypes.length > 0" v-for="(counselorType, index) in counselorTypes" :key="index">{{counselorType}}</template>
-              <template v-else>무관</template>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">작성일</th>
-            <td>{{ dayjs(counselHire.regDate).format("YYYY-MM-DD HH:mm:ss") }}</td>
-          </tr>
-          <tr v-if="counselHire.modDate !== null">
-            <th scope="row">수정일</th>
-            <td>{{ dayjs(counselHire.modDate).format("YYYY-MM-DD HH:mm:ss") }}</td>
-          </tr>
+            <tr>
+              <th scope="row">희망 상담사 나이</th>
+              <td>
+                <template v-if="counselorAgeRanges.length > 0" v-for="(counselorAgeRange, index) in counselorAgeRanges" :key="index">{{counselorAgeRange}}</template>
+                <template v-else>무관</template>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">희망 성별</th>
+              <td>{{ counselHire.hireGender }}</td>
+            </tr>
+            <tr>
+              <th scope="row">희망 조언 타입</th>
+              <td>
+                <template v-if="counselorTypes.length > 0" v-for="(counselorType, index) in counselorTypes" :key="index">{{counselorType}}</template>
+                <template v-else>무관</template>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">작성일</th>
+              <td>{{ dayjs(counselHire.regDate).format("YYYY-MM-DD HH:mm:ss") }}</td>
+            </tr>
+            <tr v-if="counselHire.modDate !== null">
+              <th scope="row">수정일</th>
+              <td>{{ dayjs(counselHire.modDate).format("YYYY-MM-DD HH:mm:ss") }}</td>
+            </tr>
           </tbody>
         </table>
+        <div class="comment-actions text-end">
+          <button v-if="sendUserSeq===userSeq" class="btn btn-sm btn-outline-secondary me-2" @click="editComment(comment)">수정</button>
+          <button v-if="sendUserSeq===userSeq" class="btn btn-sm btn-outline-danger" @click="isDelete(comment.offerSeq, comment.hireSeq)">삭제</button>
+        </div>
       </div>
 
       <!-- 댓글 -->
